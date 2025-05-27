f6522e6a754059b606dc2bd0b31db078400b4424
fvv <fyodorovvv@gmail.com>
2025-05-27 20:30:01 +0300

    terraform from yandex mirror

--- devcontainer/features/terraform/install.sh
+++ devcontainer/features/terraform/install.sh
@@ -2,10 +2,24 @@
 
 sudo apt update
 sudo apt -y install --no-install-recommends --no-install-suggests \
-    gpg wget lsb-release
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt -y install --no-install-recommends --no-install-suggests terraform
+    gpg wget lsb-release unzip
+mkdir ~/terraform && cd ~/terraform
+wget https://hashicorp-releases.yandexcloud.net/terraform/1.12.1/terraform_1.12.1_linux_386.zip
+sudo apt install unzip
+unzip terraform_1.12.1_linux_386.zip
+export PATH=$PATH:~/terraform
 terraform -install-autocomplete
-terraform init
+cat > ~/.terraformrc <<EOF
+provider_installation {
+    network_mirror {
+        url = "https://terraform-mirror.yandexcloud.net/"
+        include = ["registry.terraform.io/*/*"]
+}
+    direct {
+        exclude = ["registry.terraform.io/*/*"]
+    }
+}
+EOF
+echo 'export PATH=$PATH:~/terraform' >> ~/.bashrc
+echo 'export PATH=$PATH:~/terraform' >> ~/.profile
+#terraform init
