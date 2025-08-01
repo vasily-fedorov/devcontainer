91aaef009b605eb43d2d08714d87a66b8261abfe
fvv <fyodorovvv@gmail.com>
2025-08-01 13:37:48 +0300

    chore: update devcontainer features configuration and notes

--- devcontainer/features/features.sh
+++ devcontainer/features/features.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-bash ~/devcontainer/features/python/install.sh
+#bash ~/devcontainer/features/python/install.sh
 bash ~/devcontainer/features/direnv/install.sh
 #bash ~/devcontainer/features/litestream/install.sh
 #bash ~/devcontainer/features/dind/install.sh
-#bash ~/devcontainer/features/terraform/install.sh
-#bash ~/devcontainer/features/yandexcloud-cli/install.sh
+bash ~/devcontainer/features/terraform/install.sh
+bash ~/devcontainer/features/yandexcloud-cli/install.sh
