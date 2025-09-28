e7d8e27f60cca9b60d774b0143e27c71c3d98c8a
fvv <fyodorovvv@gmail.com>
2025-09-28 20:10:09 +0300

    feat: Add modular feature management system

--- devcontainer/features/features.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-#bash ~/devcontainer/features/python/install.sh
-bash ~/devcontainer/features/direnv/install.sh
-#bash ~/devcontainer/features/litestream/install.sh
-#bash ~/devcontainer/features/dind/install.sh
-bash ~/devcontainer/features/terraform/install.sh
-bash ~/devcontainer/features/yandexcloud-cli/install.sh
